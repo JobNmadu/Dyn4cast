90fe0457ce2e45e789c29160382665d0251138bc
Job Nda Nmadu <job.nmadu@gmail.com>
2021-02-11 22:04:55 +0100

    Changes after rd changes again

--- data-raw/Dyn4cast.R
+++ data-raw/Dyn4cast.R
@@ -1,3 +1,3 @@
 ## code to prepare `Dyn4cast` dataset goes here
-DDD <- as.data.frame(readr::read_csv("data/Data.csv"))
+Data <- readxl::read_excel("data/Data.xlsx")
 usethis::use_data(Dyn4cast, overwrite = TRUE)
