b82913cf1370afd177ebe49dc6ebac6b56c1abbb
Job Nda Nmadu <job.nmadu@gmail.com>
2021-02-11 17:43:25 +0100

    data entry procedure

--- tests/testthat/test-DynamicForecast.R
+++ tests/testthat/test-DynamicForecast.R
@@ -1,5 +1,8 @@
 test_that("DynamicForecast works", {
-  DDD <- as.data.frame(read.csv("data/day_14.csv"))
+  #DDD <- as.data.frame(read.csv("data/day_14.csv"))
+  DDD <-readxl::read_excel("data/day_14.xlsx")
+  DDD$Date <- as.Date(DDD$Date)
+
   test_model <- DynamicForecast(Data = DDD,
                                 BREAKS = c(70, 131, 173, 228, 274),
                                 Date = "2021-02-08")
