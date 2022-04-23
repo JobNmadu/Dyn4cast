21b0272122cb3d6a9cafcb20974aec08d1ed465a
Job Nda Nmadu <job.nmadu@gmail.com>
2022-04-23 16:50:39 +0100

    Mallows trouble

--- R/MallowsCp.R
+++ R/MallowsCp.R
@@ -15,11 +15,10 @@
 #' @docType package
 #'
 #' @return A list with the following components:
-#' \item{\code{Mallow's Cp}}{of the model.}
+#' \item{\code{Mallow's Cp}{of the model.}
 #'
 #' @importFrom stats anova
-#'
-#' @examples
+#' @example
 #' ctl <- c(4.17,5.58,5.18,6.11,4.50,4.61,5.17,4.53,5.33,5.14)
 #' trt <- c(4.81,4.17,4.41,3.59,5.87,3.83,6.03,4.89,4.32,4.69)
 #' x <- gl(2, 10, 20, labels = c("Ctl","Trt"))
@@ -29,7 +28,7 @@
 #'
 #' @description
 #' Compute the Mallow's Cp of the model
-MallowsCp <- function(Model, y, x, Nlevels = NULL) {
+MallowsCp <- function(Model, y, x, Nlevels = NULL){
   Anova <- anova(Model)
   AAAV <- Anova["Residuals", ]
   RSSp <- AAAV$`Sum Sq`
@@ -37,7 +36,7 @@ MallowsCp <- function(Model, y, x, Nlevels = NULL) {
   size <- length(y)
   if (is.null(ncol(x))) {
     nvars <- 1
-  } else {
+  }else{
     nvars <- ncol(x)
   }
   Cp <- RSSp / MSEp - size + 2 * (nvars + Nlevels + 1)
