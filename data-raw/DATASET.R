b35dd443fc88b562af0eb2a8b51d82953038d581
JobNmadu <job.nmadu@gmail.com>
2022-05-13 12:24:30 +0100

    data finals

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -3,13 +3,13 @@
 COVID19 <- readxl::read_excel("data-raw/Data.xlsx")
 COVID19$Date <- as.Date(COVID19$Date)
 #saveRDS(COVID19Nig, "inst/COVID19Nig.RDS")
-saveRDS(COVID19, "R/COVID19.RDS")
+saveRDS(COVID19, "inst/COVID19.RDS")
 
 usethis::use_data(COVID19, overwrite = TRUE)
 
 Data <- readxl::read_excel("data-raw/data1.xlsx")
 Data$Date <- as.Date(Data$Date)
 #saveRDS(StatesAffected, "inst/StatesAffected.RDS")
-saveRDS(Data, "R/Data.RDS")
+saveRDS(Data, "inst/Data.RDS")
 
 usethis::use_data(Data, overwrite = TRUE)
