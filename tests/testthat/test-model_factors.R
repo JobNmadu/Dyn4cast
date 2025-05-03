f200c2a96469919b6635df8d94af2c78fe83fcbf
Prof. Job N Nmadu <job.nmadu@gmail.com>
2025-05-03 15:27:58 -0400

    tests spaces

--- tests/testthat/test-model_factors.R
+++ tests/testthat/test-model_factors.R
@@ -3,10 +3,10 @@ test_that("model_factors works", {
   Data <- Quicksummary
   GGn <- names(Data)
   GG <- ncol(Data)
-  GGx <- c(paste0('x0', 1:9), paste("x", 10:ncol(Data), sep = ""))
+  GGx <- c(paste0('x0', 1 : 9), paste("x", 10 : ncol(Data), sep = ""))
   names(Data) <- GGx
-  lll <- fa.parallel(Data, fm = 'minres', fa = 'fa')
-  dat <- fa(Data, nfactors = lll[["nfact"]], rotate = "varimax", fm="minres")
+  lll <- fa.parallel(Data, fm = "minres", fa = "fa")
+  dat <- fa(Data, nfactors = lll[["nfact"]], rotate = "varimax", fm = "minres")
   test <- model_factors(data = dat, DATA = Data)
 
   Test_01 <- test[["Latent_frame"]]
