233b5d674682131f9133ccf23d5dd9eeb0ad9e1a
Prof. Job N Nmadu <job.nmadu@gmail.com>
2025-05-03 14:19:52 -0400

    Update test-treatment_model.R

--- tests/testthat/test-treatment_model.R
+++ tests/testthat/test-treatment_model.R
@@ -11,11 +11,11 @@ test_that("treatment_model works", {
   Test_18 <- test_model[["ATT plot"]]
   Test_19 <- test_model[["ATE plot"]]
 
-  expect_identical(test_model[["Model"]],Test_13)
-  expect_identical(test_model[["Residuals"]],Test_14)
-  expect_identical(test_model[["ATO plot"]],Test_15)
-  expect_identical(test_model[["ATM plot"]],Test_16)
-  expect_identical(test_model[["ATC plot"]],Test_17)
-  expect_identical(test_model[["ATT plot"]],Test_18)
-  expect_identical(test_model[["ATE plot"]],Test_19)
+  expect_identical(test_model[["Model"]], Test_13)
+  expect_identical(test_model[["Residuals"]], Test_14)
+  expect_identical(test_model[["ATO plot"]], Test_15)
+  expect_identical(test_model[["ATM plot"]], Test_16)
+  expect_identical(test_model[["ATC plot"]], Test_17)
+  expect_identical(test_model[["ATT plot"]], Test_18)
+  expect_identical(test_model[["ATE plot"]], Test_19)
 })
