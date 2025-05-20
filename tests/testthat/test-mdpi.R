94c17c2a02435325b9e5eb253cb644de50edf8aa
Prof. Job N Nmadu <job.nmadu@gmail.com>
2025-05-19 22:12:42 -0400

    Update test-mdpi.R

--- tests/testthat/test-mdpi.R
+++ tests/testthat/test-mdpi.R
@@ -12,6 +12,21 @@ test_that("mdpi works", {
   test2 <- mdpi(data, dm, plots = "t")
   Test04 <- test2
 
+  dm <- list(d1 = c("Child.Mortality", "Access.to.health.care"),
+             d2 = c("Years.of.education", "School.attendance", "School.lag"),
+             d3 = c("Cooking.Fuel", "Electricity", "Housing.Materials",
+                    "Asset.ownership"),
+             d4 = c("Access.to.clean.source.of.water",
+                    "Access.to.an.improve.sanatation"))
+
+  test5 <- mdpi(data, dm, id_add = c("Water and Sanitation"),
+                plots = "t", Factor = "Region")
+  Test17 <- test5[-1]
+  Test08 <- test5[1]
+
+  test6 <- mdpi(data, dm, id_add = c("Water and Sanitation"), plots = "t")
+  Test09 <- test6
+
   data <- mdpi2
   dm <- list(d1 = c("d_nutr","d_cm"),
              d2 = c("d_satt","d_educ"),
@@ -21,6 +36,29 @@ test_that("mdpi works", {
   Test06 <- test3[1]
   test4 <- mdpi(data, dm, plots = "t")
   Test07 <- test4
+  dm <- list(d1 = c("d_cm"),
+             d2 = c("d_satt","d_educ"),
+             d3 = c("d_elct", "d_hsg","d_ckfl","d_asst"),
+             d4 = c("d_sani","d_wtr"),
+             d5 = c("d_nutr"))
+
+  test7 <- mdpi(data, dm, id_add = "Water and Sanitation", id_add1 = "Nutrition",
+       plots = "t", Factor = "region")
+  test8 <- mdpi(data, dm, id_add = "Water and Sanitation", id_add1 = "Nutrition",
+       plots = "t")
+  dm <- list(d1 = c("d_cm"),
+             d2 = c("d_satt","d_educ"),
+             d3 = c("d_elct", "d_hsg","d_ckfl","d_asst"),
+             d4 = c("d_sani","d_wtr"),
+             d5 = c("d_nutr"))
+
+  test7 <- mdpi(data, dm, id_add = "Water and Sanitation", id_add1 = "Nutrition",
+                plots = "t", Factor = "region")
+  test8 <- mdpi(data, dm, id_add = "Water and Sanitation", id_add1 = "Nutrition",
+                plots = "t")
+  Test10 <- test7[-1]
+  Test11 <- test7[1]
+  Test12 <- test8
 
   expect_identical(test1[-1], Test01)
   expect_identical(test1[1],  Test02)
@@ -28,4 +66,10 @@ test_that("mdpi works", {
   expect_identical(test4,     Test07)
   expect_identical(test3[-1], Test05)
   expect_identical(test3[1],  Test06)
+  expect_identical(test6,     Test09)
+  expect_identical(test5[-1], Test17)
+  expect_identical(test5[1],  Test08)
+  expect_identical(test8,     Test12)
+  expect_identical(test7[-1], Test10)
+  expect_identical(test7[1],  Test11)
 })
