3475caee893303994430e965c4fabd9958f0ab96
Prof. Job N Nmadu <job.nmadu@gmail.com>
2025-05-20 18:57:33 -0400

    Update test-mdpi.R

--- tests/testthat/test-mdpi.R
+++ tests/testthat/test-mdpi.R
@@ -60,6 +60,87 @@ test_that("mdpi works", {
   Test11 <- test7[1]
   Test12 <- test8
 
+  dm <- list(d1 = c("d_cm"),
+             d2 = c("d_satt","d_educ"),
+             d3 = c("d_elct", "d_ckfl"),
+             d4 = c("d_sani","d_wtr"),
+             d5 = c("d_nutr"),
+             d6 = c("d_hsg","d_asst"))
+
+  test9 <- mdpi(data, dm, id_add = "Water and Sanitation",
+                id_add1 = "Nutrition", id_addn = "Housing and Assets",
+                plots = "t", Factor = "region")
+
+  test10 <- mdpi(data, dm, id_add = "Water and Sanitation",
+                 id_add1 = "Nutrition", id_addn = "Housing and Assets",
+                 plots = "t")
+  Test13 <- test9[-1]
+  Test14 <- test9[1]
+  Test15 <- test10
+
+  dm <- list(d1 = c("d_cm"),
+             d2 = c("d_satt","d_educ"),
+             d3 = c("d_elct", "d_ckfl"),
+             d4 = c("d_sani","d_wtr"),
+             d5 = c("d_nutr"),
+             d6 = c("d_hsg"),
+             d7 = c("d_asst"))
+
+  test11 <- mdpi(data, dm, id_add = "Water and Sanitation",
+                id_add1 = "Nutrition", id_addn = c("Housing", "Assets"),
+                plots = "t", Factor = "region")
+
+  test12 <- mdpi(data, dm, id_add = "Water and Sanitation",
+                 id_add1 = "Nutrition", id_addn = c("Housing", "Assets"),
+                 plots = "t")
+  Test16 <- test11[-1]
+  Test19 <- test11[1]
+  Test18 <- test12
+
+  dm <- list(d1 = c("d_cm"),
+             d2 = c("d_satt","d_educ"),
+             d3 = c("d_elct", "d_ckfl"),
+             d4 = c("d_sani"),
+             d5 = c("d_nutr"),
+             d6 = c("d_hsg"),
+             d7 = c("d_asst"),
+             d8 = c("d_wtr"))
+
+  test13 <- mdpi(data, dm, id_add = "Sanitation",
+                 id_add1 = "Nutrition",
+                 id_addn = c("Housing", "Assets", "Water"),
+                 plots = "t", Factor = "region")
+
+  test14 <- mdpi(data, dm, id_add = "Sanitation",
+                 id_add1 = "Nutrition",
+                 id_addn = c("Housing", "Assets", "Water"), plots = "t")
+  Test20 <- test13[-1]
+  Test21 <- test13[1]
+  Test22 <- test14
+
+  dm <- list(d1 = c("d_cm"),
+             d2 = c("d_satt","d_educ"),
+             d3 = c("d_elct", "d_ckfl"),
+             d4 = c("d_sani"),
+             d5 = c("d_nutr"),
+             d6 = c("d_hsg"),
+             d7 = c("d_asst"),
+             d8 = c("d_wtr"),
+             d9 = c("d_elct"))
+
+  test15 <- mdpi(data, dm, id_add = "Sanitation",
+                 id_add1 = "Nutrition",
+                 id_addn = c("Housing", "Assets", "Water", "Electricity"),
+                 plots = "t", Factor = "region")
+
+  test16 <- mdpi(data, dm, id_add = "Sanitation",
+                 id_add1 = "Nutrition",
+                 id_addn = c("Housing", "Assets", "Water", "Electricity"),
+                 plots = "t")
+  Test23 <- test15[-1]
+  Test24 <- test15[1]
+  Test25 <- test16
+
   expect_identical(test1[-1], Test01)
   expect_identical(test1[1],  Test02)
   expect_identical(test2,     Test04)
@@ -72,4 +153,16 @@ test_that("mdpi works", {
   expect_identical(test8,     Test12)
   expect_identical(test7[-1], Test10)
   expect_identical(test7[1],  Test11)
+  expect_identical(test10,    Test15)
+  expect_identical(test9[-1], Test13)
+  expect_identical(test9[1],  Test14)
+  expect_identical(test12,    Test18)
+  expect_identical(test11[-1], Test16)
+  expect_identical(test11[1],  Test19)
+  expect_identical(test14,    Test22)
+  expect_identical(test13[-1], Test20)
+  expect_identical(test13[1],  Test21)
+  expect_identical(test16,    Test25)
+  expect_identical(test15[-1], Test23)
+  expect_identical(test15[1],  Test24)
 })
